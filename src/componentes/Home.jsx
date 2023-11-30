799f34a23effbea55eb4c549563bd16f3268d361
sssolano <110432030+sssolano@users.noreply.github.com>
2023-11-30 13:43:32 -0600

    Cambio de funciones

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -3,16 +3,19 @@
 import React from 'react'
 import appFirebase from '../credenciales';
 import {getAuth} from 'firebase/auth';
-import CompradorUserView from '../views/CompradorUserView';
-import VendedorUserView from '../views/VendedorUserView';
+
+/* import CompradorUserView from '../views/CompradorUserView';
+import VendedorUserView from '../views/VendedorUserView';*/
+
 const auth = getAuth(appFirebase)
 
 import PaginaVendedor from './PaginaVendedor';
+import CatalogoNegocios from './CatalogoNegocios';
 
 const Home = ({usuario}) => {
   return (
     <div>
-      {usuario.rol === "vendedor" ? <CompradorUserView/> :  <PaginaVendedor/>}
+      {usuario.rol === "vendedor" ? <PaginaVendedor/> :  <CatalogoNegocios/>}
   </div>
   )
 }
