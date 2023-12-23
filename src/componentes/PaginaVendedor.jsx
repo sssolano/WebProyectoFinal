f4932362b1e0fa175292e02d1d398cf1f075efcc
maurocarlucci <mauro.carlucci.araya@gmail.com>
2023-12-23 03:29:57 -0600

    Commit que ya sirve
    ya sirve

--- src/componentes/PaginaVendedor.jsx
+++ src/componentes/PaginaVendedor.jsx
@@ -2,89 +2,100 @@ import  { useEffect, useState } from 'react';
 import { onDelete, onFindAll, onFindById, onInsert, onUpdate } from '../config/conexiones';
 import swal from 'sweetalert';
 import Formulario from './Formulario';
-import ListaContactos from './ListaContactos';
+import ListaContactos from './ListaNegocios';
 
-export const PaginaVendedor = () => {
+const PaginaVendedor = () => {
   const initialValues = {
-    nombre: "",
-    correo: "",
-    numtelefono: "",
-    direccion1:"",
-    direccion2:"",
-    tiponegocio:"",
-    codigopostal:"",
-   servicio:"",
-   canton:"",
-   distrito:"",
-   provincia:"",
-   imagen:"",
-   tag:"",
-
-
-
-  }
+    nombre: '',
+    correo: '',
+    numtelefono: '',
+    direccion1: '',
+    direccion2: '',
+    tiponegocio: '',
+    codigopostal: '',
+    servicio: '',
+    canton: '',
+    distrito: '',
+    provincia: '',
+    imagen: '',
+    tag: '',
+  };
 
   const [values, setValues] = useState(initialValues);
-  const [contactos, setContactos] = useState([]);
+  const [contactos, setNegocios] = useState([]);
   const [currentId, setCurrentId] = useState('');
 
   useEffect(() => {
-    onGetContactos();
+    onGetNegocios();
   }, []);
 
   const handleInputChange = ({ target }) => {
     const { name, value } = target;
-    setValues({ ...values, [name]: value });
-  }
+    setValues((prevValues) => ({ ...prevValues, [name]: value }));
+  };
 
-  const onGetContactos = async () => {
-    const lstContactos = await onFindAll('contactos');
-    setContactos(lstContactos.docs);
-  }
+  const onGetNegocios = async () => {
+    try {
+      const lstContactos = await onFindAll('contactos');
+      setNegocios(lstContactos.docs);
+    } catch (error) {
+      console.error('Error al obtener contactos:', error);
+    }
+  };
 
-  const onSubmit = async ev => {
+  const onSubmit = async (ev) => {
     ev.preventDefault();
     try {
       if (currentId === '') {
-        await onInsert(values);
-        swal("Registro ingresado correctamente.", { icon: "success" });
+        await onInsert('contactos', values);
+        swal('Registro ingresado correctamente.', { icon: 'success' });
       } else {
-        await onUpdate(currentId, values);
-        swal("Registro ingresado correctamente.", { icon: "success" });
+        await onUpdate('contactos', currentId, values);
+        swal('Registro modificado correctamente.', { icon: 'success' });
       }
     } catch (error) {
-      swal("Error", "Error: " + error, "error");
+      swal('Error', 'Error: ' + error.message, 'error');
     }
-    onGetContactos();
+    onGetNegocios();
     limpiar();
-  }
+  };
 
   const onDeleteContacto = ({ target }) => {
     swal({
-      title: "Desea eliminar el registro?",
-      text: "",
-      icon: "warning",
+      title: '¿Desea eliminar el registro?',
+      text: '',
+      icon: 'warning',
       buttons: true,
       dangerMode: true,
     }).then(async (willDelete) => {
       if (willDelete) {
-        await onDelete(target.dataset.id);
-        onGetContactos();
-        swal("Registro elimininado!", { icon: "success" });
+        try {
+          await onDelete('contactos', target.dataset.id);
+          onGetNegocios();
+          swal('Registro eliminado correctamente.', { icon: 'success' });
+        } catch (error) {
+          console.error('Error al eliminar contacto:', error);
+          swal('Error', 'Error al eliminar el registro.', 'error');
+        }
       }
     });
-  }
+  };
 
   const onCargarContacto = async ({ target }) => {
-    const docSeleccionado = await onFindById('contactos',target.dataset.id);
-    setValues({ ...docSeleccionado });
-    setCurrentId(target.dataset.id);
-  }
+    try {
+      const docSeleccionado = await onFindById('contactos', target.dataset.id);
+      setValues({ ...docSeleccionado });
+      setCurrentId(target.dataset.id);
+    } catch (error) {
+      console.error('Error al cargar contacto:', error);
+      swal('Error', 'Error al cargar el registro.', 'error');
+    }
+  };
 
   const limpiar = () => {
     setValues({ ...initialValues });
     setCurrentId('');
-  }
+  };
 
   return (
     <div className="container">
@@ -104,5 +115,6 @@ export const PaginaVendedor = () => {
       </div>
     </div>
   );
-}
+};
+
 export default PaginaVendedor;
