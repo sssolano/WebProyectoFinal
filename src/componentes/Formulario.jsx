f206adc1170c375b4ef58f777c7701eba8226a3c
sssolano <110432030+sssolano@users.noreply.github.com>
2023-12-23 11:00:09 -0600

    Cambio forma de Formulario

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -65,57 +65,114 @@ const Formulario = ({ values, handleInputChange, onSubmit, limpiar, currentId })
   };
 
   return (
-    <div className="col-4">
-      <h2 className='mt-2'>Mantenimiento Negocios</h2><hr />
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <input type="text" name="nombre" value={nombre} onChange={handleInputChange} placeholder='Nombre' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="correo" value={correo} onChange={handleInputChange} placeholder='correo' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="numtelefono" value={numtelefono} onChange={handleInputChange} placeholder='telefono' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="direccion1" value={direccion1} onChange={handleInputChange} placeholder='direccion' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="direccion2" value={direccion2} onChange={handleInputChange} placeholder='direccion2' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="tiponegocio" value={tiponegocio} onChange={handleInputChange} placeholder='tiponegocio' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="codigopostal" value={codigopostal} onChange={handleInputChange} placeholder='codigo postal' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="servicio" value={servicio} onChange={handleInputChange} placeholder='servicio' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="canton" value={canton} onChange={handleInputChange} placeholder='canton' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="distrito" value={distrito} onChange={handleInputChange} placeholder='distrito' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="provincia" value={provincia} onChange={handleInputChange} placeholder='provincia' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="imagen" value={imagen} onChange={handleInputChange} placeholder='imagen' className='form-control mb-1' />
-        </div>
-        <div className="form-group">
-          <input type="text" name="tag" value={tag} onChange={handleInputChange} placeholder='tag' className='form-control mb-1' />
-        </div>
-        
 
+    <div className="row">
+    <form onSubmit={handleSubmit}>
+    <h2 className='mt-2'>Creación de Negocio</h2><hr />
+  <div className="col-lg-8">
+    <div className="card mb-4">
+      <div className="card-body">
 
-
-
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Nombre</label>
+        <input type="text" name="nombre" value={nombre} onChange={handleInputChange} placeholder='Nombre' className='form-control mb-1' />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Correo</label>
+        <input type="text" name="correo" value={correo} onChange={handleInputChange} placeholder='correo' className='form-control mb-1' />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Numero de Teléfono</label>
+        <input type="text" name="numtelefono" value={numtelefono} onChange={handleInputChange} placeholder='telefono' className='form-control mb-1' />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Dirección 1</label>
+              <input type="text" name="direccion1" value={direccion1} onChange={handleInputChange} placeholder='direccion1' className='form-control mb-1'/>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Dirección 2</label>
+             <input type="text" name="direccion2" value={direccion2} onChange={handleInputChange} placeholder='direccion2' className='form-control mb-1'/>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Tipo de Negocio</label>
+         <input type="text" name="tiponegocio" value={tiponegocio} onChange={handleInputChange} placeholder='tiponegocio' className='form-control mb-1' />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Codigo Postal</label>
+     <input type="text" name="codigopostal" value={codigopostal} onChange={handleInputChange} placeholder='codigo postal' className='form-control mb-1' />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Servicio</label>
+              <input type="text" name="servicio" value={servicio} onChange={handleInputChange} placeholder='servicio' className='form-control mb-1' />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Cantón</label>
+              <input type="text" name="canton" value={canton} onChange={handleInputChange} placeholder='canton' className='form-control mb-1' />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Distrito</label>
+              <input type="text" name="distrito" value={distrito} onChange={handleInputChange} placeholder='distrito' className='form-control mb-1' />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Provincia</label>
+              <input type="text" name="provincia" value={provincia} onChange={handleInputChange} placeholder='provincia' className='form-control mb-1' />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Imagen del Negocio</label>
+              <input type="text" name="imagen" value={imagen} onChange={handleInputChange} placeholder='imagen' className='form-control mb-1' />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="mb-3">
+              <label className="form-label">Tag</label>
+               <input type="text" name="tag" value={tag} onChange={handleInputChange} placeholder='tag01' className='form-control mb-1' />
+               <input type="text" name="tag" value={tag} onChange={handleInputChange} placeholder='tag02' className='form-control mb-1' />
+               <input type="text" name="tag" value={tag} onChange={handleInputChange} placeholder='tag03' className='form-control mb-1' />
+            </div>
+          </div>
+        </div>
 
         <button className='btn btn-primary'>{currentId === '' ? 'Guardar' : 'Modificar'}</button>
-        <button type='button' className='btn btn-success mx-1' onClick={limpiar}>Limpiar</button>
-      </form>
+      <button type='button' className='btn btn-success mx-1' onClick={limpiar}>Limpiar</button>
+
+      </div>
+    </div>
+    </div>
+    </form> 
     </div>
   );
 }
